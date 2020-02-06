386c0bc87ec0f80a3c9f707d39bc24740b0a746d
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-02-06 14:40:20 +0800

    overall code

---
+++ passwordKeySystem.py
@@ -0,0 +1,31 @@
+correctPassword = 'a123456'
+
+inputValue = input('please enter the password: ')
+
+inputValue = str(inputValue)
+
+authorizationAttempts = 2
+
+if inputValue == correctPassword:
+
+	print('successful entry')
+
+else:
+
+	while authorizationAttempts >= 1:
+
+		print('unsuccessful entry, you still have', authorizationAttempts, 'attempt(s) left')
+
+		new_inputValue = input('try again: ')
+
+		new_inputValue = str(new_inputValue)
+
+		if new_inputValue == correctPassword:
+
+			print('successful entry')
+
+		else:
+
+			authorizationAttempts = authorizationAttempts - 1
+
+
